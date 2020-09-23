dd6bab0c7cb363325137661dbe40d2b126d34a36
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-09-23 22:41:20 +0530

    minor changes

--- src/main/java/com/vanshika/ecom/EcommerceApplication.java
+++ src/main/java/com/vanshika/ecom/EcommerceApplication.java
@@ -2,6 +2,13 @@ package com.vanshika.ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -9,4 +16,5 @@ public class EcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
+
 }
