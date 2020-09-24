59510e712dbdcfb126bc1a563d5cf95661202423
Ishaan Gupta <15ishaangupta@gmail.com>
2020-09-25 02:04:37 +0530

    removed eshop

--- eshop/src/main/java/com/example/eshop/repository/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eshop.repository;
-
-import com.example.eshop.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<User, Integer>{
-    User findByUsername(String username);
-    User findByUsernameAndPassword(String username, String password);
-}
---
+++ src/main/java/com/vanshika/ecom/validation/ValidPassword.java
@@ -0,0 +1,27 @@
+package com.vanshika.ecom.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+//creating annotation for password validation
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Target({ TYPE, FIELD, ANNOTATION_TYPE })
+@Retention(RUNTIME)
+public @interface ValidPassword {
+
+    String message() default "Invalid Password";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
