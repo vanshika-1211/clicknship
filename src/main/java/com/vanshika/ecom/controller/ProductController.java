3aae34dd8df851d432c820397866a5893459d6c9
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-09-27 02:53:07 +0530

    CORS handling

--- src/main/java/com/vanshika/ecom/controller/ProductController.java
+++ src/main/java/com/vanshika/ecom/controller/ProductController.java
@@ -1,7 +1,6 @@
 package com.vanshika.ecom.controller;
 
 import com.vanshika.ecom.model.Product;
-import com.vanshika.ecom.service.ProductNotFoundException;
 import com.vanshika.ecom.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ProductController {
 
     private ProductService productService;
@@ -21,30 +21,35 @@ public class ProductController {
     }
 
     @GetMapping("/allProducts")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Iterable<Product>> getProducts() {
         Iterable<Product> list=productService.getAllProducts();
         return new ResponseEntity<Iterable<Product>>(list, HttpStatus.OK);
     }
 
     @GetMapping("/productName/{name}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Iterable<Product>> getByName(@PathVariable String name) {
         Iterable<Product> list = productService.findUsingName(name);
         return new ResponseEntity<Iterable<Product>>(list, HttpStatus.OK);
     }
 
     @GetMapping("/productCategory/{category}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Iterable<Product>> getByCategory(@PathVariable String category) {
         Iterable<Product> list = productService.findUsingCategory(category);
         return new ResponseEntity<Iterable<Product>>(list, HttpStatus.OK);
     }
 
     @GetMapping("/productSubCategory/{subCategory}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Iterable<Product>> getBySubCategory(@PathVariable String subCategory) {
         Iterable<Product> list = productService.findUsingSubCategory(subCategory);
         return new ResponseEntity<Iterable<Product>>(list, HttpStatus.OK);
     }
 
-    @GetMapping("/productCategory/{cat}/ProductSubCategory/{subCat}")
+    @GetMapping("/productCategory/ProductSubCategory/{cat}/{subCat}")
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     public ResponseEntity<Iterable<Product>> getByCategoryAndSubCategory(@PathVariable("cat") String category, @PathVariable("subCat") String subCategory) {
         Iterable<Product> list = productService.findUsingCategoryAndSubCategory(category, subCategory);
         return new ResponseEntity<Iterable<Product>>(list, HttpStatus.OK);
